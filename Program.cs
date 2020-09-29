0bca2b8219d98d216584d6826e8a45893bc8aadb
David M Kinnen <davidkinnen@inspirationtrust.org>
2020-09-29 19:00:15 +0100

    Added in a function to calculate the area of a circle, and included parsing, and Math. library functions.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace CodingPracticeCSharpY13
     {
         static void Main(string[] args)
         {
+            //Coding practice for C# learning the programming basics
+            double circleArea;
+            circleArea = CalculateCircleArea();
+            Console.WriteLine($"The area of the circle is: {circleArea}");
+            Console.WriteLine($"The area of the circle is: {Math.Round(circleArea,3)}");
+            Console.ReadLine();
+        }
+
+        static double CalculateCircleArea()
+        {
+            double circleArea;
+            int radius;
+            const double VALUE_OF_PI = 3.14;
+            
+
+            Console.WriteLine("What is the radius of the circle?");
+            radius = Int32.Parse(Console.ReadLine());
+
+            //circleArea = VALUE_OF_PI * radius * radius;
+            circleArea = Math.PI * Math.Pow(radius, 2);
+
+
+            return circleArea;
         }
     }
 }
